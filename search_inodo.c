2b220766e9ce0c0e2bf311077e1ad064fcd28f72
A6575 <astridaalvaradoa@gmail.com>
2023-04-17 06:43:23 -0400

    documentación 100%

--- search_inodo.c
+++ search_inodo.c
@@ -13,35 +13,38 @@ void print_info_file(char *file, struct flags act_flags, long int size){
 	}
 }
 
-// Funcion que dice si el directorio existe o no.
+// Funcion que dice si el directorio existe o en su defecto es posible realizar el recorrido por los niveles.
 int directory_exits(int level, char *folder, char *goal, struct flags act_flags) {
 	char *temp;
-	// Si el primer archivo es un folder con el nombre del objetivo o si no se pidio filtrar por region regresa true.
+	// Si el primer archivo es un folder con el nombre del objetivo o si no se pidio filtrar por region regresa true
+	// para indicar que es posible seguir buscando.
 	if (level == 0 && (strcmp(folder, goal) == 0 || !act_flags.Region)){
 		return TRUE;
 	}
-	// Si el segundo archivo es un folder con el nombre del objetivo o si no se pidio filtrar por especie regresa true.
+	// Si el segundo archivo es un folder con el nombre del objetivo o si no se pidio filtrar por especie regresa true
+	// para indicar que es posible seguir buscando.
 	else if (level == 1 && (strcmp(folder, goal) == 0 || !act_flags.Species)){
 		return TRUE;
 	}
 	// Si el tercer archivo es un folder con el nombre del objetivo o si no se pidio filtrar por tipo de personaje regresa true.
+	// para indicar que es posible seguir buscando.
 	else if (level == 2 && (strcmp(folder, goal) == 0 || !act_flags.Type)){
 		return TRUE;
 	}
-	// Caso en el que se busca el cuarto archivo.
+	// Caso en el que se busca el cuarto archivo, quiere decir que se llegó al nivel correspondiente a los archivos HTML..
 	else if (level == 3){
 		// Si no se tiene un nombre para filtrar regresar true.
 		if (!act_flags.Name){
 			return TRUE;
 		}
-		// Crear variable vacia.
+		// Crear variable del tamaño del nombre a filtrar, inicializada en 0.
 		temp = calloc((int)(strlen(goal)), sizeof(char));
 
 		// Si temp es NULL ocurrio un error y no se puede asignar memoria.
 		if (temp == NULL){
 			perror("Could not allocate memory");
 		}
-		// Copiar nombre del folder actual en temp.
+		// Copiar nombre del folder actual en temp con el fin .
 		strncpy(temp, folder, strlen(goal));
 
 		// Revisar si nombre de temp es igual al del folder buscado.
@@ -56,14 +59,7 @@ int directory_exits(int level, char *folder, char *goal, struct flags act_flags)
 	return FALSE;
 }
 
-/*Función que permite navegar por todos los archivos y directorios de un archivo.
- *Argumentos:
- *	dir_name: nombre del directorio a buscar.
- *		  dir_name = "." indica que se buscará iniciando desde la carpeta actual.
- *
- *	act_flags: flags activas al momento de usar el programa. Condicionan la búsqueda.
- */
-
+//Función que permite navegar por todos los archivos y directorios de un archivo siguiendo el criterio de búsqueda dado por el usuario.
 void search_directories(char *dir_name, struct flags act_flags, int level, int *count, long int *total_size)
 {
 	DIR *directory;
@@ -74,21 +70,24 @@ void search_directories(char *dir_name, struct flags act_flags, int level, int *
 
 	// abrimos el directorio.
 	directory = opendir(dir_name);
-
+	
+	// en caso de que el directory retorne NULL, se indica que hubo un error al abrir el archivo
 	if (directory == NULL){
 		printf("Could not open directory\n");
 		return;
 	}
-
+	
+	//cambiamos el directorio al actual
 	chdir(dir_name);
-
+	
+	//se inicializa la variabel goal como 0
 	goal = calloc(64, sizeof(char));
 	if (goal == NULL){
 		printf("Could not allocate memory");
 		return;
 	}
 
-	// leemos las entradas del directorio.
+	// leemos las entradas del directorio, dependiendo del nivel o flag activo se asigna la meta.
 	if (level == 0 && act_flags.Region){
 		strcpy(goal, act_flags.info_region);
 	}
@@ -101,13 +100,14 @@ void search_directories(char *dir_name, struct flags act_flags, int level, int *
 	else if (act_flags.Name){
 		strcpy(goal, act_flags.info_name);
 	}
-
+	
+	//recorremos por todos los archivos y directorios de la carpeta actual
 	while ((entry = readdir(directory)) != NULL){
-		// Obviar carpeta actual y anterior.
+		// Obviar carpeta actual y anterior para evitar posibles bucles.
 		if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0){
 			continue;
 		}
-		// Revisar si el directorio existe,
+		// Revisar si el directorio existe o es posible seguir buscando por los directorios,
 		if (directory_exits(level, entry->d_name, goal, act_flags) == TRUE){
 			fd = open(entry->d_name, O_RDONLY);
 			// Caso donde archivo no puede abrirse.
@@ -119,7 +119,8 @@ void search_directories(char *dir_name, struct flags act_flags, int level, int *
 			else if (fstat(fd, &status) == -1){
 				printf("Error: can't get the file information of: %s\n", entry->d_name);
 			}
-
+			
+			//si la entrada actual es de tipo DIRECTORIO
 			if (S_ISDIR(status.st_mode)){
 				// se deben obviar la carpeta actual y la anterior,
 				if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0){
@@ -129,6 +130,8 @@ void search_directories(char *dir_name, struct flags act_flags, int level, int *
 				search_directories(entry->d_name, act_flags, level + 1, count, total_size);
 			}
 			else{
+				//una vez llegado al nivel correspondiente a los archivos HTML, procesamos la informacion siempre y
+				//cuando el archivo sea del formato .html
 				if (strstr(entry->d_name, ".html") != NULL){
 					// Mostrar data del archivo actual.
 					print_info_file(entry->d_name, act_flags, status.st_size / 1024);
